84f2ec387f44a04a96af5db8762488327f830704
yolandaPing <yolandaping1224@gmail.com>
2026-08-11 11:54:06 +0800

    path dir

--- scripts/py37/saveRootPath.py
+++ scripts/py37/saveRootPath.py
@@ -5,17 +5,30 @@
 # .@Author:You P
 # .
 # .Finish time:
-
-
 import os
 
+
+def get_py_rigassit_dirs():
+    """
+    查找当前工具所在 PY_RIGASSIT 目录
+    """
+    current_dir = os.path.dirname(__file__)
+    scripts_dir = os.path.dirname(current_dir)
+    utility_dir = os.path.dirname(scripts_dir)
+    py_rigassit_root = utility_dir
+    icons_dir = os.path.join(py_rigassit_root, "icons")
+    curve_data_dir = os.path.join(py_rigassit_root, "files", "CurveData")
+    return py_rigassit_root, scripts_dir, current_dir, icons_dir, curve_data_dir
+
+
+py_rigassit_dir, scripts_dir, current_dir, icons_dir, curve_data_dir = get_py_rigassit_dirs()
 
 ParentPath = os.path.split(__file__)[0].split('scripts')[0]
 ScriptsPath = os.path.split(__file__)[0].split(' ')[0]
 IconsPath = ParentPath+"icons"
 CurveDataPath = ParentPath+"files/CurveData"
 
-# print(os.path.dirname(__file__).split('scripts')[0], '')
-# print(os.path.join(os.path.dirname(__file__), ''))
 
 # print(ParentPath,ScriptsPath,IconsPath,CurveDataPath)
+
+# print("py_rigassit:", get_py_rigassit_root())
